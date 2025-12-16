46c2d44617c4405c87f4034c58f34b1ac6c55d97
Jayasree Varadarajan <140917755+jaypython13@users.noreply.github.com>
2025-12-16 18:32:09 +0000

    Update query.py

--- query.py
+++ query.py
@@ -9,7 +9,8 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 #llama = Ollama(model="llama3:3.2")  # Update as needed
-llamma = Ollama(model="llama3:70b") 
+#llamma = Ollama(model="llama3:70b") 
+llama = Ollama(model="llama3:8b")
 # Entity extraction prompt and chain
 entity_extract_prompt = PromptTemplate(
     input_variables=["answer_text"],
